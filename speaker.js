450ae6d40c11a6f299097cf62e455fe672938d92
Igor <romanov.fortech@gmail.com>
2023-11-09 17:13:21 +0300

    Fix: translate to es6 class. Add: some methods

--- speaker.js
+++ speaker.js
@@ -1,4 +1,4 @@
-var WebBrowser = {
+var WebBrowser = Object.freeze({
   BrowserName: {
     Unknown: 'Unknown',
     InternetExplorer: 'InternetExplorer',
@@ -10,129 +10,103 @@ var WebBrowser = {
     Konqueror: 'Konqueror',
     Linx: 'Linx',
   },
-};
-Object.freeze(WebBrowser);
+});
 
-function Speaker() {
-  var Speaker = this;
+class Speaker {
+  constructor() {
+    this.speakerId = null;
+    this.appr = false;
+    this.ot = ['Cobol', 'Punch Cards', 'Commodore', 'VBScript'];
+    this.domains = ['aol.com', 'hotmail.com', 'prodigy.com', 'CompuServe.com'];
+    this.emps = ['Microsoft', 'Google', 'Fog Creek Software', '37Signals'];
+  }
 
-  //Register a speaker and return speakerID
-  this.register = function (repository) {
-    //let's init some variables
-    var speakerId = null;
-    var good = false;
-    var appr = false;
+  checkData() {
+    if (!this.firstName.trim()) {
+      throw new Error('First Name is required');
+    } else if (!this.lastName.trim()) {
+      throw new Error('Last Name is required');
+    } else if (!this.email.trim()) {
+      throw new Error('Email is required');
+    }
+  }
 
-    //var nt = ["Angular 8", ".Net Core 3", "NodeJs", "TypeScript, "Kotlin"];
-    var ot = ['Cobol', 'Punch Cards', 'Commodore', 'VBScript'];
+  calculateFee() {
+    if (this.exp <= 1) {
+      this.registrationFee = 500;
+    } else if (this.exp <= 3) {
+      this.registrationFee = 250;
+    } else if (this.exp <= 5) {
+      this.registrationFee = 100;
+    } else if (this.exp <= 9) {
+      this.registrationFee = 50;
+    } else {
+      this.registrationFee = 0;
+    }
+  }
 
-    //DEFECT #5274 weren't filtering out prodigy domain so I added it.
-    var domains = ['aol.com', 'hotmail.com', 'prodigy.com', 'CompuServe.com'];
+  register = (repository) => {
+    var emailParts = this.email.split('@');
+    var emailDomain = emailParts[emailParts.length - 1];
+    this.checkData()
+    this.isStatusGood =
+      this.exp > 10 ||
+      this.hasBlog ||
+      this.certifications.length > 3 ||
+      this.emps.includes(this.employer) ||
+      (!this.domains.includes(emailDomain) &&
+        !(
+          this.browser.name ==
+          WebBrowser.BrowserName.InternetExplorer &&
+          this.browser.majorVersion < 9
+        ))
 
-    if (Speaker.firstName !== null && Speaker.firstName.trim() !== '') {
-      if (Speaker.lastName !== null && Speaker.lastName.trim() !== '') {
-        if (Speaker.email !== null && Speaker.email.trim() !== '') {
-          //list of employers
-          var emps = ['Microsoft', 'Google', 'Fog Creek Software', '37Signals'];
-
-          //DFCT #838 Jimmy
-          //We're now requiring 3 certificatons so I changes the hard coded number
-          good =
-            Speaker.exp > 10 ||
-            Speaker.hasBlog ||
-            Speaker.certifications.length > 3 ||
-            emps.indexOf(Speaker.employer) !== -1;
-
-          if (!good) {
-            //need to get just the domain from the email
-            var emailParts = Speaker.email.split('@');
-            var emailDomain = emailParts[emailParts.length - 1];
+    if (this.isStatusGood) {
+      if (this.sessions.length !== 0) {
+        // if (this.sessions.some(session => session.title.includes(tech) || session.description.includes(tech))) {
+        //   session.approved = false;
+        // } else {
+        //   session.aprroved = true;
+        //   this.appr = true;
+        // }
+        
+          for (var tech of this.ot) {
             if (
-              domains.indexOf(emailDomain) === -1 &&
-              !(
-                Speaker.browser.name ==
-                  WebBrowser.BrowserName.InternetExplorer &&
-                Speaker.browser.majorVersion < 9
-              )
+              session.title.includes(tech) ||
+              session.description.includes(tech)
             ) {
-              good = true;
+              session.approved = false;
+              break;
+            } else {
+              session.aprroved = true;
+              this.appr = true;
             }
           }
+       
+      } else {
+        throw new Error(
+          "Can't register speaker with no sessions to present."
+        );
+      }
 
-          if (good) {
-            //DEFECT #5013 CO 1/12/2012
-            //We weren't requiring at least one session
-            if (Speaker.sessions.length !== 0) {
-              for (var session of Speaker.sessions) {
-                //for (var tech of nt)
-                //{
-                //    if (session.title.indexOf(tech) !== -1)
-                //    {
-                //        session.approved = true;
-                //        break;
-                //    }
-                //}
-                for (var tech of ot) {
-                  if (
-                    session.title.indexOf(tech) !== -1 ||
-                    session.description.indexOf(tech) !== -1
-                  ) {
-                    session.approved = false;
-                    break;
-                  } else {
-                    session.aprroved = true;
-                    appr = true;
-                  }
-                }
-              }
-            } else {
-              throw new Error(
-                "Can't register speaker with no sessions to present."
-              );
-            }
+      if (this.appr) {
 
-            if (appr) {
-              //if we got this far, the speaker is approved
-              //let's go ahead and register him/her now.
-              //First, let's calculate the registration fee.
-              //More experienced speakers pay a lower fee.
-              if (Speaker.exp <= 1) {
-                Speaker.registrationFee = 500;
-              } else if (Speaker.exp >= 2 && Speaker.exp <= 3) {
-                Speaker.registrationFee = 250;
-              } else if (Speaker.exp >= 4 && Speaker.exp <= 5) {
-                Speaker.registrationFee = 100;
-              } else if (Speaker.exp >= 6 && Speaker.exp <= 9) {
-                Speaker.registrationFee = 50;
-              } else {
-                Speaker.registrationFee = 0;
-              }
+        this.calculateFee();
 
-              //Now, save the speaker and sessions to the db.
-              try {
-                speakerId = repository.saveSpeaker(this);
-              } catch (e) {
-                //in case the db call fails
-              }
-            } else {
-              throw new Error('No sessions approved.');
-            }
-          } else {
-            throw new Error(
-              "Speaker doesn't meet our abitrary and capricious standards."
-            );
-          }
-        } else {
-          throw new Error('Email is required');
+        try {
+          this.speakerId = repository.saveSpeaker(this);
+        } catch (e) {
+          //in case the db call fails
         }
       } else {
-        throw new Error('Last Name is required');
+        throw new Error('No sessions approved.');
       }
     } else {
-      throw new Error('First Name is required');
+      throw new Error(
+        "Speaker doesn't meet our abitrary and capricious standards."
+      );
     }
-
-    return speakerId;
+    return this.speakerId;
   };
 }
 
